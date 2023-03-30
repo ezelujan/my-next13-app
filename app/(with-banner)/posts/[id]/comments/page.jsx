a03321f3b5769e5a5d9cbff207adaab351674c85
Ezequiel Luján Escobar <ojos1098@gmail.com>
2023-03-29 22:20:48 -0500

    feat: next/image

--- app/(with-banner)/posts/[id]/comments/page.jsx
+++ app/(with-banner)/posts/[id]/comments/page.jsx
@@ -1,6 +1,6 @@
-const fetchComments = async id => {
-  await new Promise(resolve => setTimeout(resolve, 1500))
+import Image from 'next/image'
 
+const fetchComments = async id => {
   const res = await fetch(
     `https://jsonplaceholder.typicode.com/posts/${id}/comments`,
     {
@@ -20,6 +20,12 @@ export default async function Post ({ params }) {
     <ul style={{ background: '#444', fontSize: '11px' }}>
       {comments.map(comment => (
         <li key={comment.id}>
+          <Image
+            height={50}
+            width={50}
+            alt={comment.name}
+            src={`https://api.dicebear.com/6.x/big-smile/svg?seed=${comment.email}`}
+          />
           <h2>{comment.name}</h2>
           <p>{comment.body}</p>
         </li>
--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   experimental: {
     appDir: true
+  },
+  images: {
+    domains: ['api.dicebear.com'],
+    dangerouslyAllowSVG: true
   }
 }
 
